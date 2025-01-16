242d31a3d988089c12d319ff85cd61b382ac889c
Hea092024 <Hea092024@gmail.com>
2025-01-16 13:48:02 +0100

    .

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 const button = document.querySelector("button");
 
+function greet(){
+    const name = prompt("what is your name?");
+    const greeting = querySelector("#greeting");
+    greeting.textContent = `Hello ${name}, nice to see you"`;
+}
+
+button.addEventListener("click", greet);
