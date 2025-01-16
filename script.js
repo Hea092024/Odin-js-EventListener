84082dc7fbc64501d2139aa60c5d86734b30b00f
Hea092024 <Hea092024@gmail.com>
2025-01-16 13:57:59 +0100

    .

--- script.js
+++ script.js
@@ -7,3 +7,12 @@ function greet(){
 }
 
 button.addEventListener("click", greet);
+
+const song = "Fight the Youth";
+const score = 9;
+const highestScore = 10;
+const output = `I like the song ${song}. I gave it a score of ${
+  (score / highestScore) * 100
+}%.`;
+console.log (output);
+
